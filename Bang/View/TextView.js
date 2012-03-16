05560b6b718abd43f68f81a748835baca5de7618
Schell Scivally <schell.scivally@synapsegroup.com>
2012-03-16 16:46:13 -0700

    updated readme added text stub

---
+++ Bang/View/TextView.js
@@ -0,0 +1,8 @@
+/** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* TextView.js
+* The TextView addin.
+* Copyright (c) 2012 Schell Scivally. All rights reserved.
+* 
+* @author    Schell Scivally
+* @since    Fri Mar 16 16:14:15 PDT 2012
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
