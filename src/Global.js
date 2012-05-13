929b3bb13f1f181e3cf065bbffebe4d13d868fa8
Schell Scivally <schell.scivally@synapsegroup.com>
2012-05-13 15:03:56 -0700

    Global is trimmed down.

--- src/Global.js
+++ src/Global.js
@@ -1,83 +1,25 @@
 /** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *    Global.js
- *    A set of functions all of Bang needs.
- *    Copyright (c) 2012 . All rights reserved.
- *
- *    @author    Schell Scivally
- *    @since    Tue Jan 10 20:05:33 PST 2012
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
+*    Global.js
+*    A set of functions added right into the top modules object.
+*    Copyright (c) 2012 . All rights reserved.
+*
+*    @author    Schell Scivally
+*    @since    Tue Jan 10 20:05:33 PST 2012
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
 mod({
     name : 'Global',
     dependencies : [],
+    /**
+    * Initializes Global.
+    * Defines functions inside mod's module object that all of Bang will need.
+    * @param - m Object - mod's module object.
+    */
     init : function initGlobal (m) {
         /**
-         * Initializes Global.
-         * Defines functions inside mod's module object that all of Bang will need.
-         * @param - m Object - mod's module object.
-         */
-         var defined = m.defined = function (variable) {
-            /**
-             * Determines whether or not the variable is defined.
-             * @param - variable Object - The object to test the definition of.
-             * @return - Boolean
-             */
-            if (variable === null) {
-                return false;
-            }
-            if (variable === undefined) {
-                return false;
-            }
-            return true;
-        };
-        
-        var ifndefInit = m.ifndefInit = function (variable, value) {
-            /**
-             *    Ensures that a variable is defined, sets variable to value
-             *    if not, returns variable.
-             *    @return variable
-             */
-            variable = defined(variable) ? variable : value;
-            return variable;
-        };
-
-        var ifndefInitNum = m.ifndefInitNum = function (variable, value) {
-            /**
-             *    Ensures that variable is defined as a number, defaults to value.
-             *    @return Number
-             */
-            variable = m.defined(variable) ? variable : value;
-            if (isNaN(variable)) {
-                if (isNaN(value)) {
-                    console.error('value',value.toString(),'is not a number...');
-                    return 0;
-                } else {
-                    variable = value;
-                }
-            }
-            return variable;
-        };
-
-        var ifndefInitObj = m.ifndefInitObj = function (obj, defaultObj) {
-            /**
-             *    Ensures that *obj* is defined, and has (at least) all properties of *defaultObj*.
-             *    *defaultObj* should be the default state of *obj*.
-             *    @param  obj - an object, the one to init
-             *    @param    defaultObj - an object, the default object
-             *    @return obj - an object, the one init'd
-             */
-            obj = obj || defaultObj;
-            for (var key in defaultObj) {
-                if (!(key in obj) || key === 'toString') {
-                    obj[key] = defaultObj[key];
-                }
-            }
-            return obj;
-        };
-
-        var assert = m.assert = (function () {
-            /**
-             * An object that facilitates unit tests.
-             */
+        * An object that facilitates unit tests.
+        * @type {object}
+        */
+        m.assert = (function () {
             var passes = 0, fails = 0;
             var failStrings = [];
             var tests = [];
@@ -125,14 +67,17 @@ mod({
             };
         })();
         
-        // A private variable to hold all animations...
+        /** * *
+        * A private array to hold all the current animations.
+        * @type {Array.<object>}
+        * * **/
         var _animations = [];
+        /** * *
+        * Calls *animationFunction* over and over again.
+        * @param - animationFunction Function - The function to call over and over, repeatedly.
+        * @returns - Function
+        * * **/
         m.requestAnimation = function (animationFunction) {
-            /** * *
-            * Calls *animationFunction* over and over again.
-            * @param - animationFunction Function - The function to call over and over, repeatedly.
-            * @returns - Function
-            * * **/
             var request = window.requestAnimationFrame       ||
                           window.webkitRequestAnimationFrame ||
                           window.mozRequestAnimationFrame    ||
@@ -149,12 +94,12 @@ mod({
             
             // Store a reference to the animation...
             _animations.push(animation);
-            
+
+            /** * *
+            * Calls the animation function and schedules another call.
+            * @param {number}
+            * * **/
             var animate = function (time) {
-                /**
-                 * Calls the animation function and schedules another call.
-                 * 
-                 */
                 time = time || Date.now();
                 
                 if (animation.cancelled !== false) {
@@ -170,12 +115,12 @@ mod({
             animation.id = request(animate);
             return animation;
         };
-        
+
+        /** * *
+        * Cancels an animation request returned by m.requestAnimation.
+        * @param {object}
+        * * **/
         m.cancelAnimation = function(animation) {
-            /** * *
-            * Cancels an animation request returned by m.requestAnimation.
-            * @param - animationFunction Function
-            * * **/
             var timeout = function(id) {
                 clearTimeout(id);
             };
@@ -190,101 +135,22 @@ mod({
                 _animations.splice(ndx, 1);
             }
         };
-        
+
+        /** * *
+        * Cancels all animations currently registered.
+        * * **/
         m.cancelAllAnimations = function() {
-            /** * *
-            * Cancels all animations currently registered.
-            * * **/
             while (_animations.length) {
                 m.cancelAnimation(_animations[0]);
             }
         };
-        
+
+        /** * *
+        * Returns the number of registered animations.
+        * @return {number}
+        * * **/
         m.animationCount = function() {
-            /** * *
-            * Returns the number of registered animations.
-            * @returns - Number
-            * * **/
             return _animations.length;
-        };
-        
-        m.safeAddin = function safeAddin(obj, key, value) {
-            /**
-             * Adds *value* to *obj* as the property *key*, unless *key*
-             * already exists on *obj*.
-             * @param - obj Object - The object to add *value* to.
-             * @param - key String - The property name of *value*.
-             * @param - value Object - Some object to add to *obj*.
-             */
-            if (!(key in obj)) {
-                obj[key] = value;
-                return true;
-            }
-            return false;
-        };
-        
-        m.safeAddinAllPropertiesOf = function safeAddinAllPropertiesOf(obj, addin) {
-            /** * *
-            * Adds in all the properties of *addin* onto *obj*, in a safe way.
-            * @param Object The object to add properties to.
-            * @param Object The object to add properties from.
-            * @return Returns the first parameter after adding properties from the second.
-            * * **/
-            for (var key in addin) {
-                m.safeAddin(obj, key, addin[key]);
-            }
-        }
-        
-        m.safeOverride = function safeOverride(obj, key, superKey, value) {
-            /** * *
-            * Adds *value* to *obj* as the property *key*. If *key*
-            * already exists the existing *key* is stored on *obj*
-            * as *superKey*.
-            * @param - obj Object - The object to add *value* to.
-            * @param - key String - The property name of *value*.
-            * @param - superKey String - The new property name of the super value.
-            * @param - value Object - Some object to add to *obj*.
-            * * **/
-            if (key.toString() in obj) {
-                var superValue = obj[key];
-                obj[key] = value;
-                return m.safeAddin(obj, superKey, superValue);
-            } else {
-                obj[key] = value;
-            }
-        };
-        
-        // a unique id for created objects.
-        var _uniqueId = 0;
-        m.Object = function (self) {
-            /**
-             * Creates an initial object used in Addins. 
-             * @param - self Object
-             * @return - Object
-             */
-            if (!m.defined(self)) {
-                self = {};
-            }
-            
-            var uniqueId = _uniqueId++;
-            
-            self.toString = function () {
-                var tag = self.tag || uniqueId;
-                return '['+tag.toString()+']';
-            };
-            
-            self.uniqueId = function () {
-                return uniqueId;
-            };
-            
-            return self;
-        };
-        
-        m.objectIsKindOfObject = function(object, kindOf) {
-            /** * *
-            * TODO: Finish this by comparing properties.
-            * * **/
-            return false;
         };
 
         (function Global_setupLogging() {
@@ -299,21 +165,6 @@ mod({
             window.console.trace = function () {};
         })();
 
-        (function Global_implementObjectCreate() {
-            /**
-             *    Provides prototypal inheritance for all objects.
-             *
-             *    @author Douglas Crockford
-             */
-            if (typeof Object.create !== 'function') {
-                Object.create = function (o) {
-                    function F() {}
-                    F.prototype = o;
-                    return new F();
-                };
-            }
-        })();
-        // return the global object
         return {};
     }
 });
